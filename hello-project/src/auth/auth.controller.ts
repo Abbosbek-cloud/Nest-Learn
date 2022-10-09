492b469b3d4d9732b8312b77cd274f89c40d2ff2
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-10 00:01:04 +0500

    authorization created

--- hello-project/src/auth/auth.controller.ts
+++ hello-project/src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(): String {
+    return 'I signed up';
+  }
+
+  @Post('login')
+  login(): String {
+    return 'I signed in';
+  }
 }
