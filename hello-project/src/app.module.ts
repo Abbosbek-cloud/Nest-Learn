dfad5ec11091114cc170e5d480c207ec990ac39e
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-09 23:56:52 +0500

    modules services and controllers created

--- hello-project/src/app.module.ts
+++ hello-project/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user-module/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
 })
 export class AppModule {}
