015c377d65c654bfc9bfe021ce2478e2fa210ed0
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-10 23:50:15 +0500

    modules are connected

--- hello-project/src/app.module.ts
+++ hello-project/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ProductModule],
 })
 export class AppModule {}
